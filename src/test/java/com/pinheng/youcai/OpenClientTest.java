98b1676a432be5057e6e86df0a0449ebe3374c0e
Cai Jiang <luffy.ja@gmail.com>
2023-05-25 10:53:17 +0800

    加入 yanxuan.product.details

--- src/test/java/com/pinheng/youcai/OpenClientTest.java
+++ src/test/java/com/pinheng/youcai/OpenClientTest.java
@@ -1,10 +1,14 @@
 package com.pinheng.youcai;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +21,9 @@ class OpenClientTest {
     void dummy() throws OpenException, IOException {
         OpenClient client = new OpenClient();
         client.setTargetEndpoint("http://b2b.test.you.163.com/");
-        val token = client.grantAccessToken("9u5fwqu6", "a28a53e7f2deb3cb9a038d95d1600827");
+        String key = "9u5fwqu6";
+        String secret = "a28a53e7f2deb3cb9a038d95d1600827";
+        val token = client.grantAccessToken(key, secret);
         assertThat(token)
                 .as("通过测试环境肯定可以成功获取token")
                 .satisfies(it -> {
@@ -31,7 +37,7 @@ class OpenClientTest {
                             .isGreaterThan(0);
                 });
 
-        val token2 = client.refreshAccessToken("9u5fwqu6", token.getRefreshToken());
+        val token2 = client.refreshAccessToken(key, token.getRefreshToken());
         assertThat(token2)
                 .as("通过测试环境肯定可以成功刷新token")
                 .satisfies(it -> {
@@ -46,13 +52,27 @@ class OpenClientTest {
                 });
 
         val listAllSpuResult
-                = client.executeApi("9u5fwqu6", "a28a53e7f2deb3cb9a038d95d1600827", token2.getAccessToken()
+                = client.executeApi(key, secret, token2.getAccessToken()
                 , "yanxuan.product.listAllSpu", "1.0.0", null);
 
         assertThat(listAllSpuResult)
                 .as("切不管这个测试用户是否拥有商品，但是必须得返回")
                 .isNotNull()
                 .satisfies(it -> log.info(it.toString()));
+
+        val listAllResult
+                = client.executeApi(key, secret, token2.getAccessToken()
+                , "yanxuan.product.listAll", "1.0.0", null);
+        System.out.println(listAllResult);
+        val list =
+                StreamSupport.stream(((ArrayNode) listAllResult)
+                                .spliterator(), true)
+                        .map(JsonNode::textValue)
+                        .collect(Collectors.joining(","));
+        System.out.println(list);
+        System.out.println(client.executeApi(key, secret, token2.getAccessToken()
+                , "yanxuan.product.details", "1.0.0", list));
+
 
     }
 
